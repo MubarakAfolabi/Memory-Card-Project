dd7105a286c40938760121b6bfccc11a4e60f995
Mubarak Afolabi <mubarakbolu150@gmail.com>
2025-04-17 10:12:40 +0100

    Imported an image and implemented it in the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
+import headerImg from "./assets/headerImg.png";
 
-function App() {}
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <img src={headerImg} />
+        <span className="header-title">JOJO Memory Card</span>
+      </header>
+    </div>
+  );
+}
 
 export default App;
