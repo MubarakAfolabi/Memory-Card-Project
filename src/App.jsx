4450085826a61e3f4bff4a38e7426d4717751a4b
Mubarak Afolabi <mubarakbolu150@gmail.com>
2025-04-18 10:26:22 +0100

    Imported arr and Implemented it in the itemArr useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import "./App.css";
+import { useState } from "react";
 import headerImg from "./assets/headerImg.png";
-import tusk from "./assets/grid/tusk.webp";
-import echoes from "./assets/grid/echoes.webp";
-import heavensDoor from "./assets/grid/heavens-door.webp";
-import d4c from "./assets/grid/d4c.webp";
-import wOfU from "./assets/grid/wOfu.webp";
-import stickyFingers from "./assets/grid/sticky-fingers.webp";
-import theWorld from "./assets/grid/the-world.webp";
-import starPlatinum from "./assets/grid/star-platinum.webp";
-import theHand from "./assets/grid/the-hand.webp";
-import killerQueen from "./assets/grid/killer-queen.webp";
-import softAndWet from "./assets/grid/soft-and-wet.webp";
-import goldenExperience from "./assets/grid/golden-experience.webp";
+import arr from "./itemArray.js";
+import "./App.css";
 
 function App() {
+  const [itemArr, setItemArr] = useState([...arr]);
+
   return (
     <div className="body-container">
       <div className="dark-container">
@@ -29,114 +21,19 @@ function App() {
         </header>
         <main>
           <div className="grid-container">
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={tusk} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Tusk Act 4</p>
-                <p className="stand-cry">"chumimi~in"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={echoes} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Echoes Act 3</p>
-                <p className="stand-cry">"S-H-I-T"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={heavensDoor} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Heaven's Door</p>
-                <p className="stand-cry">"Hebunzu Doā"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={d4c} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Dirty Deeds Done Dirt Cheap</p>
-                <p className="stand-cry">"Dī Fō Shī -Rabu Torein"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={wOfU} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Wonder of U</p>
-                <p className="stand-cry">"Flow is always a calamity."</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={stickyFingers} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Sticky Fingers</p>
-                <p className="stand-cry">"ARI, ARI, ARI, ARI, Arrivederci! "</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={theWorld} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">The World</p>
-                <p className="stand-cry">"Muda Muda Muda"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={starPlatinum} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Star Platinum</p>
-                <p className="stand-cry">"Ora Ora Ora"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={theHand} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">The Hand</p>
-                <p className="stand-cry">"Za Hando"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={killerQueen} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Killer Queen</p>
-                <p className="stand-cry">"Bites za Dusto"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={softAndWet} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Soft and Wet</p>
-                <p className="stand-cry">"Ora Ora Ora"</p>
-              </div>
-            </div>
-            <div className="grid-item">
-              <div className="image-container">
-                <img src={goldenExperience} />
-              </div>
-              <div className="name-container">
-                <p className="stand-name">Golden Experience</p>
-                <p className="stand-cry">"Muda Muda Muda"</p>
-              </div>
-            </div>
+            {itemArr.map((item) => {
+              return (
+                <div key={item.id} className="grid-item">
+                  <div className="image-container">
+                    <img src={item.imgName} />
+                  </div>
+                  <div className="name-container">
+                    <p className="stand-name">{item.standName}</p>
+                    <p className="stand-cry">"{item.standCry}"</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </main>
       </div>
---
+++ src/itemArray.js
@@ -0,0 +1,89 @@
+import tusk from "./assets/grid/tusk.webp";
+import echoes from "./assets/grid/echoes.webp";
+import heavensDoor from "./assets/grid/heavens-door.webp";
+import d4c from "./assets/grid/d4c.webp";
+import wOfU from "./assets/grid/wOfu.webp";
+import stickyFingers from "./assets/grid/sticky-fingers.webp";
+import theWorld from "./assets/grid/the-world.webp";
+import starPlatinum from "./assets/grid/star-platinum.webp";
+import theHand from "./assets/grid/the-hand.webp";
+import killerQueen from "./assets/grid/killer-queen.webp";
+import softAndWet from "./assets/grid/soft-and-wet.webp";
+import goldenExperience from "./assets/grid/golden-experience.webp";
+
+const arr = [
+  {
+    id: 1,
+    imgName: tusk,
+    standName: "Tusk Act 4",
+    standCry: "chumimi~in",
+  },
+  {
+    id: 2,
+    imgName: echoes,
+    standName: "Echoes Act 3",
+    standCry: "S-H-I-T",
+  },
+  {
+    id: 3,
+    imgName: heavensDoor,
+    standName: "Heaven's Door",
+    standCry: "Hebunzu Doā",
+  },
+  {
+    id: 4,
+    imgName: d4c,
+    standName: "Dirty Deeds Done Dirt Cheap",
+    standCry: "Dī Fō Shī -Rabu Torein",
+  },
+  {
+    id: 5,
+    imgName: wOfU,
+    standName: "Wonder of U",
+    standCry: "Flow is always a calamity",
+  },
+  {
+    id: 6,
+    imgName: stickyFingers,
+    standName: "Sticky Fingers",
+    standCry: "ARI, ARI, ARI, ARI, Arrivederci!",
+  },
+  {
+    id: 7,
+    imgName: theWorld,
+    standName: "The World",
+    standCry: "Muda Muda Muda",
+  },
+  {
+    id: 8,
+    imgName: starPlatinum,
+    standName: "Star Platinum",
+    standCry: "Ora Ora Ora",
+  },
+  {
+    id: 9,
+    imgName: theHand,
+    standName: "The Hand",
+    standCry: "Za Hando",
+  },
+  {
+    id: 10,
+    imgName: killerQueen,
+    standName: "Killer Queen",
+    standCry: "Bites za Dusto",
+  },
+  {
+    id: 11,
+    imgName: softAndWet,
+    standName: "Soft and Wet",
+    standCry: "Ora Ora Ora",
+  },
+  {
+    id: 12,
+    imgName: goldenExperience,
+    standName: "Golden Experience",
+    standCry: "Muda Muda Muda",
+  },
+];
+
+export default arr;
